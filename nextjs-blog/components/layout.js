3e10410117f5bc40477f680bab533827f2101acb
Rena <choco.rena0609@gmail.com>
2020-06-02 08:15:28 +0900

    動的ルーティング

--- nextjs-blog/components/layout.js
+++ nextjs-blog/components/layout.js
@@ -1,6 +1,7 @@
 import styles from "./layout.module.css";
-import Head from 'next/head'
+import Head from "next/head";
 import utilStyles from "../styles/utils.module.css";
+import Link from "next/link";
 
 const name = "Rena";
 export const siteTitle = "Next.js Sample Website";
